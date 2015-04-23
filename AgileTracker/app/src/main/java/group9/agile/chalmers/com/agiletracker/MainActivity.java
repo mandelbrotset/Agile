5a2c4a8375e75c87ba1d96414d1d2aa42889744b
alma-castant <alma.castillo.antolin@gmail.com>
2015-04-23 13:30:16 +0200

    GitHub first connection

--- AgileTracker/app/src/main/java/group9/agile/chalmers/com/agiletracker/MainActivity.java
+++ AgileTracker/app/src/main/java/group9/agile/chalmers/com/agiletracker/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import group9.agile.chalmers.com.agiletracker.network.GitHub;
+
 
 public class MainActivity extends ActionBarActivity {
 
@@ -12,6 +14,8 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new GitHub().execute("alma-castant", "pinteresting");
     }
 
 
